216d37e6b7e7a49bb7fb2e552ef102c186ae9543
sabbir <programmarsabbir@gmail.com>
2026-04-04 22:04:40 +0800

    complete-hero-section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
 export default function Home() {
--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,8 +1,27 @@
 // I have creating navbar by default
-import React from "react";
+import { Briefcase } from "lucide-react";
+import Link from "next/link";
+import { Button } from "./button";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="border-b border-gray-200 bg-white">
+      <div className="container mx-auto flex h-16 items-center px-4 justify-between">
+        <Link href="/" className="flex gap-2 items-center text-xl font-semibold text-pink-500">
+          <Briefcase />
+          Job Tracker
+        </Link>
+        <div className="flex gap-3 items-center ">
+          <Link href="sign-in">
+            <Button className="hover:text-white bg-pink-500 p-5">Log In</Button>
+          </Link>
+          <Link href="sign-up">
+            <Button className="bg-pink-500 p-5">Start for free</Button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
